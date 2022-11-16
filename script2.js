27af4dcd74f32dac5587a405b510aaf694285782
Alberto De Armas <ajdearmas13@gmail.com>
2022-11-16 13:27:24 -0500

    editing html to include dark theme

--- script2.js
+++ script2.js
@@ -34,10 +34,11 @@ saveBtn.on('click', function(event) {
 //Gets item from localStorage to be used in my code
 let userInputFromLS =  localStorage.getItem('cityData')
 
+//*** 5 day forcast call***/
 //Building string to use in my fetch
 let api5dayForecast = 'http://api.openweathermap.org/data/2.5/forecast?q=' + userInputFromLS + '&units=imperial&appid=8b70d97613f963c4f438360db59ebce1'
 
-//It uses my constructed string to pull data from yhe 5day API
+//It uses my constructed string to pull data from the 5day API
 function fetchingApi5day () {
     fetch(api5dayForecast)
     .then(response => response.json())
@@ -47,77 +48,96 @@ function fetchingApi5day () {
 }
 fetchingApi5day()
 
+//*** Current weather call***/
+//Building string to use in my fetch
+let apiCurrentWeather = 'https://api.openweathermap.org/data/2.5/weather?q=' + userInputFromLS + '&units=imperial&appid=4f702383a74cebe940df686df1fd83c2'
+
+//It uses my constructed string to pull data from the current API
+function fetchingApiCurrentday () {
+    fetch(apiCurrentWeather)
+    .then(response => response.json())
+    .then(data => {
+      apiCurrentCityRes = data;
+    });
+}
+fetchingApiCurrentday ()
 
 // After the data is fetched, then start adding elements from the fetch to the cards
 //Card 1
 //Add City Name
-card1h4City1.append(apiCityRes.city.name)
+card1h4City1.append(apiCurrentCityRes.name)
 //Add Date 
-card1h5Date1.append(apiCityRes.list[0].dt_txt.split(' ')[0])
+card1h5Date1.append(dayjs().format('YYYY-MM-DD')+' - Today')
 //Add the temperature
-$('#card-1 li#l1-1').append(apiCityRes.list[0].main.temp)
+$('#card-1 li#l1-1').append(apiCurrentCityRes.main.temp + '°F')
 //Add the humidity
-$('#card-1 li#l1-2').append(apiCityRes.list[0].main.humidity + ' %')
+$('#card-1 li#l1-2').append(apiCurrentCityRes.main.humidity + ' %')
 //Add the MPH
-$('#card-1 li#l1-3').append(apiCityRes.list[0].wind.deg + ' MPH')
+$('#card-1 li#l1-3').append(apiCurrentCityRes.wind.speed + ' MPH')
 
 
 
 //Card 2
-// add image
+// include specific image to a variable
 let imageCard2 = 'http://openweathermap.org/img/wn/' + apiCityRes.list[6].weather[0].icon + '@2x.png'
-//Check
-$('#card-2 img').attr('src',imageCard2);
+// add image to HTML
+$('#card-2 img').attr('src',imageCard2).css('width', '50px');
 //Add City Name
 card2h4City2.append(apiCityRes.city.name)
 //Add Date 
 card2h5Date2.append(apiCityRes.list[6].dt_txt.split(' ')[0])
 //Add the temperature
-$('#card-2 li#l2-1').append(apiCityRes.list[6].main.temp)
+$('#card-2 li#l2-1').append(apiCityRes.list[6].main.temp + '°F')
 //Add the humidity
 $('#card-2 li#l2-2').append(apiCityRes.list[6].main.humidity + ' %')
 //Add the MPH
-$('#card-2 li#l2-3').append(apiCityRes.list[6].wind.deg + ' MPH')
+$('#card-2 li#l2-3').append(apiCityRes.list[6].wind.speed + ' MPH')
 
 //Card 3
-// add image
+// include specific image to a variable
 let imageCard3 = 'http://openweathermap.org/img/wn/' + apiCityRes.list[14].weather[0].icon + '@2x.png'
+// add image to HTML
+$('#card-3 img').attr('src',imageCard3).css('width', '50px');
 //Add City Name
 card3h4City3.append(apiCityRes.city.name)
 //Add Date 
 card3h5Date3.append(apiCityRes.list[14].dt_txt.split(' ')[0])
 //Add the temperature
-$('#card-3 li#l3-1').append(apiCityRes.list[14].main.temp)
+$('#card-3 li#l3-1').append(apiCityRes.list[14].main.temp + '°F')
 //Add the humidity
 $('#card-3 li#l3-2').append(apiCityRes.list[14].main.humidity + ' %')
 //Add the MPH
-$('#card-3 li#l3-3').append(apiCityRes.list[14].wind.deg + ' MPH')
+$('#card-3 li#l3-3').append(apiCityRes.list[14].wind.speed + ' MPH')
 
 
 //Card 4
 // add image
 let imageCard4 = 'http://openweathermap.org/img/wn/' + apiCityRes.list[22].weather[0].icon + '@2x.png'
+// add image to HTML
+$('#card-4 img').attr('src',imageCard4).css('width', '50px');
 //Add City Name
 card4h4City4.append(apiCityRes.city.name)
 //Add Date 
 card4h5Date4.append(apiCityRes.list[22].dt_txt.split(' ')[0])
 //Add the temperature
-$('#card-4 li#l4-1').append(apiCityRes.list[22].main.temp)
+$('#card-4 li#l4-1').append(apiCityRes.list[22].main.temp + '°F')
 //Add the humidity
 $('#card-4 li#l4-2').append(apiCityRes.list[22].main.humidity + ' %')
 //Add the MPH
-$('#card-4 li#l4-3').append(apiCityRes.list[22].wind.deg + ' MPH')
+$('#card-4 li#l4-3').append(apiCityRes.list[22].wind.speed + ' MPH')
 
 //Card 5
 // add image
 let imageCard5 = 'http://openweathermap.org/img/wn/' + apiCityRes.list[30].weather[0].icon + '@2x.png'
+// add image to HTML
+$('#card-5 img').attr('src',imageCard5).css('width', '50px');
 //Add City Name
 card5h4City5.append(apiCityRes.city.name)
 //Add Date 
 card5h5Date5.append(apiCityRes.list[30].dt_txt.split(' ')[0])
 //Add the temperature
-$('#card-5 li#l5-1').append(apiCityRes.list[30].main.temp)
+$('#card-5 li#l5-1').append(apiCityRes.list[30].main.temp + '°F')
 //Add the humidity
 $('#card-5 li#l5-2').append(apiCityRes.list[30].main.humidity + ' %')
 //Add the MPH
-$('#card-5 li#l5-3').append(apiCityRes.list[30].wind.deg + ' MPH')
+$('#card-5 li#l5-3').append(apiCityRes.list[30].wind.speed + ' MPH')
